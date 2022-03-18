d06aaf84bbf425a71f58bcffce7e54794eb33bc0
norahosny66 <norahosny039@gmail.com>
2022-03-18 17:14:31 +0200

    Add project files.

---
+++ DB_project/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace DB_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.check_btn = new System.Windows.Forms.Button();
+            this.txt_admin_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.show_price = new System.Windows.Forms.Button();
+            this.insert_btn = new System.Windows.Forms.Button();
+            this.meal_name_cmb = new System.Windows.Forms.ComboBox();
+            this.meal_price = new System.Windows.Forms.TextBox();
+            this.meal_name_label = new System.Windows.Forms.Label();
+            this.price_label = new System.Windows.Forms.Label();
+            this.meal_type = new System.Windows.Forms.Label();
+            this.type_txt = new System.Windows.Forms.TextBox();
+            this.txt_admin_name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // check_btn
+            // 
+            this.check_btn.Location = new System.Drawing.Point(268, 74);
+            this.check_btn.Name = "check_btn";
+            this.check_btn.Size = new System.Drawing.Size(75, 23);
+            this.check_btn.TabIndex = 1;
+            this.check_btn.Text = "Check";
+            this.check_btn.UseVisualStyleBackColor = true;
+            this.check_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_admin_id
+            // 
+            this.txt_admin_id.Location = new System.Drawing.Point(141, 48);
+            this.txt_admin_id.Name = "txt_admin_id";
+            this.txt_admin_id.Size = new System.Drawing.Size(100, 20);
+            this.txt_admin_id.TabIndex = 3;
+            this.txt_admin_id.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Admin_id";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "admin_name";
+            // 
+            // show_price
+            // 
+            this.show_price.Location = new System.Drawing.Point(407, 330);
+            this.show_price.Name = "show_price";
+            this.show_price.Size = new System.Drawing.Size(99, 23);
+            this.show_price.TabIndex = 6;
+            this.show_price.Text = "show meal price";
+            this.show_price.UseVisualStyleBackColor = true;
+            this.show_price.Click += new System.EventHandler(this.show_price_Click);
+            // 
+            // insert_btn
+            // 
+            this.insert_btn.Location = new System.Drawing.Point(298, 330);
+            this.insert_btn.Name = "insert_btn";
+            this.insert_btn.Size = new System.Drawing.Size(75, 23);
+            this.insert_btn.TabIndex = 7;
+            this.insert_btn.Text = "insert meal";
+            this.insert_btn.UseVisualStyleBackColor = true;
+            this.insert_btn.Click += new System.EventHandler(this.insert_btn_Click);
+            // 
+            // meal_name_cmb
+            // 
+            this.meal_name_cmb.FormattingEnabled = true;
+            this.meal_name_cmb.Location = new System.Drawing.Point(338, 174);
+            this.meal_name_cmb.Name = "meal_name_cmb";
+            this.meal_name_cmb.Size = new System.Drawing.Size(121, 21);
+            this.meal_name_cmb.TabIndex = 8;
+            // 
+            // meal_price
+            // 
+            this.meal_price.Location = new System.Drawing.Point(338, 219);
+            this.meal_price.Name = "meal_price";
+            this.meal_price.Size = new System.Drawing.Size(100, 20);
+            this.meal_price.TabIndex = 9;
+            this.meal_price.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // meal_name_label
+            // 
+            this.meal_name_label.AutoSize = true;
+            this.meal_name_label.Location = new System.Drawing.Point(265, 177);
+            this.meal_name_label.Name = "meal_name_label";
+            this.meal_name_label.Size = new System.Drawing.Size(58, 13);
+            this.meal_name_label.TabIndex = 10;
+            this.meal_name_label.Text = "meal name";
+            // 
+            // price_label
+            // 
+            this.price_label.AutoSize = true;
+            this.price_label.Location = new System.Drawing.Point(265, 226);
+            this.price_label.Name = "price_label";
+            this.price_label.Size = new System.Drawing.Size(55, 13);
+            this.price_label.TabIndex = 11;
+            this.price_label.Text = "meal price";
+            // 
+            // meal_type
+            // 
+            this.meal_type.AutoSize = true;
+            this.meal_type.Location = new System.Drawing.Point(265, 269);
+            this.meal_type.Name = "meal_type";
+            this.meal_type.Size = new System.Drawing.Size(52, 13);
+            this.meal_type.TabIndex = 12;
+            this.meal_type.Text = "meal type";
+            this.meal_type.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // type_txt
+            // 
+            this.type_txt.Location = new System.Drawing.Point(338, 262);
+            this.type_txt.Name = "type_txt";
+            this.type_txt.Size = new System.Drawing.Size(100, 20);
+            this.type_txt.TabIndex = 13;
+            // 
+            // txt_admin_name
+            // 
+            this.txt_admin_name.Location = new System.Drawing.Point(141, 95);
+            this.txt_admin_name.Name = "txt_admin_name";
+            this.txt_admin_name.Size = new System.Drawing.Size(100, 20);
+            this.txt_admin_name.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_admin_name);
+            this.Controls.Add(this.type_txt);
+            this.Controls.Add(this.meal_type);
+            this.Controls.Add(this.price_label);
+            this.Controls.Add(this.meal_name_label);
+            this.Controls.Add(this.meal_price);
+            this.Controls.Add(this.meal_name_cmb);
+            this.Controls.Add(this.insert_btn);
+            this.Controls.Add(this.show_price);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_admin_id);
+            this.Controls.Add(this.check_btn);
+            this.Name = "Form1";
+            this.Text = "9";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.form_closing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button check_btn;
+        private System.Windows.Forms.TextBox txt_admin_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button show_price;
+        private System.Windows.Forms.Button insert_btn;
+        private System.Windows.Forms.ComboBox meal_name_cmb;
+        private System.Windows.Forms.TextBox meal_price;
+        private System.Windows.Forms.Label meal_name_label;
+        private System.Windows.Forms.Label price_label;
+        private System.Windows.Forms.Label meal_type;
+        private System.Windows.Forms.TextBox type_txt;
+        private System.Windows.Forms.TextBox txt_admin_name;
+    }
+}
+
---
+++ DB_project/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+
+namespace DB_project
+{
+    public partial class Form1 : Form
+    {
+        int check = 0;
+        string ordb = "Data source=orcl;User Id=hr;Password=hr;";
+        OracleConnection conn;
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)//check_btn
+        {
+            //proc change check
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "check_admin";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("id", txt_admin_id.Text);
+            cmd.Parameters.Add("name", txt_admin_name.Text);
+            cmd.Parameters.Add("r",OracleDbType.Int32 ,ParameterDirection.Output);
+            cmd.ExecuteNonQuery();
+            int cnt =Convert.ToInt32(cmd.Parameters["r"].Value.ToString());    //Convert.ToString(cmd.Parameters["role"].Value.ToString());
+            //check = 1; //
+            if(cnt == 1  )  
+            {
+                meal_name_cmb.Enabled = true;
+                meal_price.Enabled = true;
+                meal_name_label.Enabled = true;
+                price_label.Enabled = true;
+                insert_btn.Enabled = true;
+                show_price.Enabled = true;
+                meal_type.Enabled = true;
+                type_txt.Enabled = true;
+
+                OracleCommand cmd2 = new OracleCommand();
+                cmd2.Connection = conn;
+                cmd2.CommandText = "select meal_name from meal";
+                cmd2.CommandType = CommandType.Text;
+                OracleDataReader dr = cmd2.ExecuteReader();
+                while(dr.Read())
+                {
+                    meal_name_cmb.Items.Add(dr[0]);
+                }
+            }
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            txt_admin_name.Text = check.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new OracleConnection(ordb);
+            conn.Open();
+            meal_name_cmb.Enabled = false;
+            meal_price.Enabled = false;
+            meal_name_label.Enabled = false;
+            price_label.Enabled = false;
+            insert_btn.Enabled = false;
+            show_price.Enabled = false;
+            meal_type.Enabled = false;
+            type_txt.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_btn_Click(object sender, EventArgs e)
+        {
+            //generate new id       cmd.txt
+            int max_id, new_id;
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "GetMealID";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("id", OracleDbType.Int32, ParameterDirection.Output);
+            cmd.ExecuteNonQuery();
+            try
+            {
+                max_id = Convert.ToInt32(cmd.Parameters["id"].Value.ToString());
+                new_id = max_id + 1;
+            }
+            catch
+            {
+                new_id = 1;
+            }
+
+            //call insert       proc
+            OracleCommand cmd2 = new OracleCommand();
+            cmd2.Connection = conn; ; ;
+            cmd2.CommandText = "insert into meal (meal_id, meal_name, meal_price,  meal_type)   values(:new_id , :name , :price,:type)";
+            cmd2.CommandType = CommandType.Text;
+            cmd2.Parameters.Add("id", new_id);
+            cmd2.Parameters.Add("name", meal_name_cmb.Text);
+            cmd2.Parameters.Add("price", meal_price.Text);
+            cmd2.Parameters.Add("type", type_txt.Text);
+
+            int r = cmd2.ExecuteNonQuery();
+            if(r != -1)
+            {
+                meal_name_cmb.Items.Add(meal_name_cmb.Text);
+                MessageBox.Show("meal added");
+                meal_name_cmb.Text = "";
+                meal_price.Text = "";
+                type_txt.Text = "";
+            }
+
+        }
+
+        private void show_price_Click(object sender, EventArgs e)
+        {
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "show_meal_price";
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("name", meal_name_cmb.SelectedItem.ToString());
+            cmd.Parameters.Add("price", OracleDbType.Int32, ParameterDirection.Output);
+            int r = cmd.ExecuteNonQuery();
+
+            /*
+             OracleCommand cmd2 = new OracleCommand();
+            cmd2.Connection = conn;
+            cmd2.CommandText = "show_meal_type";
+            cmd2.CommandType = CommandType.StoredProcedure;
+            cmd2.Parameters.Add("name", meal_name_cmb.SelectedItem.ToString());
+            cmd2.Parameters.Add("type", OracleDbType.Varchar2, ParameterDirection.Output);
+            int r2 = 1; //cmd2.ExecuteNonQuery();
+            */
+            //meal_price.Text = "zz";
+            meal_price.Text = (cmd.Parameters["price"].Value.ToString());
+            
+            cmd.ExecuteNonQuery();
+
+            /*if (r!= -1 )
+            {
+                //type_txt.Text = (cmd2.Parameters["type"].Value.ToString());
+            }*/
+            /*
+             if (r2 != -1)
+            {
+               // meal_price.Text = (cmd.Parameters["price"].Value.ToString());
+                type_txt.Text = (cmd2.Parameters["type"].Value.ToString());
+            }*/
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void form_closing(object sender, FormClosedEventArgs e)
+        {
+            conn.Dispose();
+        }
+
+        private void meal_name_cmb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
